f675fc6dd6256e8e6657f4f8d7addfc485d84ea5
munens <kaumbuthom@yahoo.com>
2018-01-30 15:52:02 -0800

    Sec. 4, lec. 34, 36-37: Connect and Provider, Header setup, React Router setup

--- src/components/app.js
+++ src/components/app.js
@@ -1,9 +1,13 @@
 import React, { Component } from 'react';
+import Header from './header';
 
 export default class App extends Component {
   render() {
     return (
-      <div>React simple starter</div>
+      <div>
+        <Header />
+        {this.props.children /* used for react router to know that if it has any children it should match those route with the correct children. */}
+      </div>
     );
   }
 }
---
+++ src/components/resources.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+
+const ResourceList = () => {
+	return (
+		<div>
+			Super special recipe
+			<ul>
+				<li>1 Cup sugar</li>
+				<li>1 Cup pepper</li>
+				<li>1 Cup salt</li>
+			</ul>
+		</div>
+	);
+}
+
+export default ResourceList;
