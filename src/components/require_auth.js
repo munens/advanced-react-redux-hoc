600e7b0d1935047676f358933120a91e7acad187
munens <kaumbuthom@yahoo.com>
2018-01-30 17:31:35 -0800

    Sec. 2, lec. 41: HOC Scaffold code

--- src/components/require_auth.js
+++ src/components/require_auth.js
@@ -3,9 +3,24 @@ import React, { Component } from 'react';
 export default function(ComposedComponent){
 	class Authentication extends Component {
 		render(){
+			console.log(this.props.resources);
 			return <ComposedComponent {...this.props} />
 		}
 	}
 
 	return Authentication;
 }
+
+// In some other location (not in this file), we'd like to use this HOC above:
+
+import Authentication // This is my HOC
+import ResourceList // This is the component I want to wrap.
+
+
+const ComposedComponent = Authentication(ResourseList);
+
+
+// In some render function: - I am running the:
+<ComposedComponent resources={resources} />
+
+// Higher order component is a function called with our existing component that returns an existing component.
