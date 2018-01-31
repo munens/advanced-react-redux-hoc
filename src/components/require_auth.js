8f54f16e52105565a78fe0747e9e0e30073ee40e
munens <kaumbuthom@yahoo.com>
2018-01-30 18:24:51 -0800

    Sec. 2, lec. 42-45: Nesting higher order components, accessing react router on context, class level properties, Handling HOC edge cases

--- src/components/require_auth.js
+++ src/components/require_auth.js
@@ -3,8 +3,24 @@ import { connect } from 'react-redux';
 
 export default function(ComposedComponent){
 	class Authentication extends Component {
+
+		static contextTypes = {
+			router: React.PropTypes.object
+		}
+
+		componentWillMount(){
+			if(!this.props.authenticated){
+				this.context.router.push('/');
+			}
+		}
+
+		componentWillUpdate(nextProps) {
+			if(!nextProps.authenticated){
+				this.context.router.push('/');
+			}
+		}
+
 		render(){
-			console.log('rendering composed component', this.props)
 			return <ComposedComponent {...this.props} />
 		}
 	}
